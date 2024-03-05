80fddcc96af9f32cc347d4a67049bc6882fc4e84
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2024-03-05 18:33:38 +0100

    Encapsulate chatbot behind a login button

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 import streamlit as st
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import auth
+from firebase_admin import credentials, auth
 from chatbot.model import handle_query
-from chatbot.data import faq_data, bursary_data, registry_data, courses_data, exam_schedule_data, school_fees_data
-
-
-
+from chatbot.data import faq_data, bursary_data, registry_data, courses_data
 
 # Initialize Firebase app
 if not firebase_admin._apps:
-  cred = credentials.Certificate("babcock-6b68d-firebase-adminsdk-zakzq-be757502db.json")
-  default_app = firebase_admin.initialize_app(cred)
-
-
+    cred = credentials.Certificate("babcock-6b68d-firebase-adminsdk-zakzq-be757502db.json")
+    default_app = firebase_admin.initialize_app(cred)
 
 # Get a list of predefined questions from the FAQ data
 predefined_questions = list(faq_data.keys())
@@ -21,22 +15,14 @@ bursary_predefined_questions = list(bursary_data.keys())
 registry_predefined_questions = list(registry_data.keys())
 courses_predefined_questions = list(courses_data.keys())
 
-
-
 def welcome_user():
     st.title("Babcock FAQ Chatbot")
-
 
 def display_answer(topic):
     response = handle_query(topic)
     st.write(f"**Answer:** {response}")
-    # if "DM" in response:
-    #     st.write(f"**Answer:** All of our staffs are busy, someone will get back to you shortly")
-    #     # send topic to staff
-
 
 def qa_bot():
-
     # Display predefined questions as a selectbox or multiselect
     courses_question = st.selectbox("Some questions people ask about Courses", courses_predefined_questions)
     if courses_question:
@@ -51,66 +37,44 @@ def qa_bot():
     if user_query:
         display_answer(user_query)
 
-
-
-
 def login(email, password):
     def authenticate_user():
         try:
             user = auth.get_user_by_email(email)
-            # auth.verify_password(email=email, password=password)
-            print(user.uuid)
-            # st.session_state.user_id = user.uid
-            # st.session_state.user_email = user.email
-            # st.session_state.is_user_logged_in = True
             st.session_state.username = user.uid
             st.session_state.useremail = user.email
             st.session_state.is_user_logged_in = True
             st.success("Login Successful")
             qa_bot()
-
-            # st.session_state.username = user.uid
-            # st.session_state.user_email = user.email
-            # st.session_state.signed_out = True
-            # st.session_state.sign_out = True
-            # st.write("Login Successful")
-        except:
+            # Display signup button
+            if st.button('Sign Up'):
+                # Call function to display signup form
+                display_signup_form()
+        except Exception as e:
             st.warning('Login Failed')
+            st.error(f"Error: {e}")
     return authenticate_user()
 
-
-# def display_login_signup():
-#     choice = st.selectbox('Login/Signup', ['Login', 'Sign up'])
-#     email = st.text_input('Email Address')
-#     password = st.text_input('Password', type='password')
-
-#     if choice == 'Sign up':
-#         username = st.text_input("Enter your unique username")
-#         if st.button('Create my account'):
-#           try:
-#             auth.create_user(email=email, password=password, uid=username)
-#             st.success('Account created successfully!')
-#             st.markdown('Please Login using your email and password')
-#             st.balloons()
-#           except:
-#             st.warning("User already exists!")
-
-#     else:
-#         st.button('Login', on_click=lambda: login(email, password))
-
-
-
+def display_signup_form():
+    username = st.text_input("Enter your unique username")
+    email = st.text_input('Email Address')
+    password = st.text_input('Password', type='password')
+    if st.button('Create my account'):
+        try:
+            auth.create_user(email=email, password=password, uid=username)
+            st.success('Account created successfully!')
+            st.markdown('Please Login using your email and password')
+            st.balloons()
+        except Exception as e:
+            st.warning("User creation failed!")
+            st.error(f"Error: {e}")
 
 def main():
-  welcome_user()
-  qa_bot()
-    # display_login_signup()
-    # display_user_info()
-
-
-
-
-  
+    welcome_user()
+    email = st.text_input('Email Address')
+    password = st.text_input('Password', type='password')
+    if st.button('Login', on_click=lambda: login(email, password)):
+        pass
 
 if __name__ == "__main__":
     main()
