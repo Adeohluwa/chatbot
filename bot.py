bded68088cafb1d0cb9512c9f624f7a71767efa5
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2024-03-14 21:31:29 +0100

    Add ask admin, save feedback and enquiries

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
+import time
+from db import auth, save_feedback, save_enquiry
 import streamlit as st
+from streamlit_star_rating import st_star_rating
+
 from chatbot.model import handle_query
 from chatbot.data import faq_data, bursary_data, registry_data, courses_data
 
@@ -10,7 +14,7 @@ predefined_questions = list(faq_data.keys())
 bursary_predefined_questions = list(bursary_data.keys())
 registry_predefined_questions = list(registry_data.keys())
 courses_predefined_questions = list(courses_data.keys())
-
+# hostel_predefined_questions =
 
 
 
@@ -18,7 +22,6 @@ courses_predefined_questions = list(courses_data.keys())
 def display_answer(topic):
     response = handle_query(topic)
     st.write(f"**Answer:** {response}")
-
 
 
 
@@ -33,6 +36,63 @@ def qa_bot():
     if bursary_question:
         display_answer(bursary_question)
 
+    registry_question = st.selectbox("Some questions people ask about Registry", bursary_predefined_questions)
+    if registry_question:
+        display_answer(registry_question)
+
     user_query = st.text_input("Ask a question")
     if user_query:
         display_answer(user_query)
+
+# Ask Admin button
+    if st.button("Ask Admin") or st.session_state.get("ask_admin"):
+        st.session_state["ask_admin"] = True
+        st.write("What would you like to ask the administrative staff?")
+        enquiry_text = st.text_area("Enquiry")
+
+        if st.button("Send Message"):
+            user = st.session_state["user"]
+            decoded_token = auth.verify_id_token(user["idToken"])
+            uid = decoded_token['uid'] 
+            print(uid)
+           
+            save_enquiry(uid, enquiry_text)
+
+            st.success("Message received, a staff will reach out to you shortly!")
+            time.sleep(2)
+            st.session_state["ask_admin"] = False
+            del enquiry_text
+            st.rerun()
+
+
+
+
+
+    # Star rating
+    stars = st_star_rating("Please take some time to rate your experience", maxValue=5, defaultValue=3, key="rating")
+
+    # Track whether the stars have been clicked
+    if stars != 3:
+        # User feedback form
+        st.write("Please provide your feedback:")
+        feedback_text = st.text_area("Feedback")
+
+        # Submit button
+        if st.button("Submit Feedback"):
+            user = st.session_state["user"]
+            decoded_token = auth.verify_id_token(user["idToken"])
+            uid = decoded_token['uid'] 
+            print(uid)
+           
+            save_feedback(uid, feedback_text, stars)
+            st.success("Thank you for your feedback!")
+
+
+
+
+    # Logout button
+    if st.button("Logout"):
+        del st.session_state["user"]
+        st.warning("You have been logged out")
+        time.sleep(2)
+        st.rerun()
